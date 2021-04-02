f78077d24e4d3d65e8d738c2057a550dd48e3051
Cameron St. Amant <cameronstamant@protonmail.com>
2021-04-02 13:26:35 -0600

    Add item creation form

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,5 +1,6 @@
 const Item = require('../models/item');
 const Category = require('../models/category');
+const { body, validationResult } = require('express-validator');
 
 exports.item_list = (req, res, next) => {
   Item.find().exec((err, list_items) => {
@@ -23,26 +24,74 @@ exports.item_detail = async (req, res, next) => {
   });
 };
 
-exports.item_create_get = function (req, res, next) {
-  res.send('Item create GET');
+exports.item_create_get = (req, res, next) => {
+  Category.find().exec((err, list_categories) => {
+    if (err) {
+      return next(err);
+    }
+    res.render('item_form', {
+      title: 'Create Item',
+      categories: list_categories,
+    });
+  });
 };
 
-exports.item_create_post = function (req, res, next) {
-  res.send('Item create POST');
-};
+exports.item_create_post = [
+  body('name')
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage('Item name must be specified.'),
+  body('description')
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage('Item description must be specified.'),
 
-exports.item_delete_get = function (req, res, next) {
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.render('item_form', {
+        title: 'Create Item',
+        name: req.body.name,
+        description: req.body.description,
+        category: req.body.category,
+        price: req.body.price,
+        number_in_stock: req.body.number_in_stock,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      let item = new Item({
+        name: req.body.name,
+        description: req.body.description,
+        category: req.body.category,
+        price: req.body.price,
+        number_in_stock: req.body.number_in_stock,
+      });
+      item.save((err) => {
+        if (err) {
+          return next(err);
+        }
+        res.redirect(item.url);
+      });
+    }
+  },
+];
+
+exports.item_delete_get = (req, res, next) => {
   res.send('Item delete GET');
 };
 
-exports.item_delete_post = function (req, res, next) {
+exports.item_delete_post = (req, res, next) => {
   res.send('Item delete POST');
 };
 
-exports.item_update_get = function (req, res, next) {
+exports.item_update_get = (req, res, next) => {
   res.send('Item update GET');
 };
 
-exports.item_update_post = function (req, res, next) {
+exports.item_update_post = (req, res, next) => {
   res.send('Item update POST');
 };
--- routes/item.js
+++ routes/item.js
@@ -6,7 +6,7 @@ const item = require('../models/item');
 
 router.get('/create', item_controller.item_create_get);
 
-router.get('/create', item_controller.item_create_post);
+router.post('/create', item_controller.item_create_post);
 
 router.get('/', item_controller.item_list);
 
@@ -14,10 +14,10 @@ router.get('/:id', item_controller.item_detail);
 
 router.get('/:id/delete', item_controller.item_delete_get);
 
-router.get('/:id/delete', item_controller.item_delete_post);
+router.post('/:id/delete', item_controller.item_delete_post);
 
 router.get('/:id/update', item_controller.item_update_get);
 
-router.get('/:id/update', item_controller.item_update_post);
+router.post('/:id/update', item_controller.item_update_post);
 
 module.exports = router;
