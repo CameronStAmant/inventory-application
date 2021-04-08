ea5cd532364d22bafed056c2bfc8e86c6e8ba9eb
Cameron St. Amant <cameronstamant@protonmail.com>
2021-04-08 14:41:29 -0600

    fix variables pulling from incorrect db

--- controllers/wikiController.js
+++ controllers/wikiController.js
@@ -2,8 +2,8 @@ const Item = require('../models/item');
 const Category = require('../models/category');
 
 exports.index = async (req, res, next) => {
-  let categoryCount = await Item.countDocuments({});
-  let itemCount = await Category.countDocuments({});
+  let categoryCount = await Category.countDocuments({});
+  let itemCount = await Item.countDocuments({});
   res.render('index', {
     title: 'Welcome!',
     categoryTotal: categoryCount,
